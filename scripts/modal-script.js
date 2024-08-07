f6dc88687a59529d96d9b7ed6a542d6301dc88a3
ShabanovaNatalya <natalikurilova@icloud.com>
2024-08-07 06:51:34 +0400

    2 commit

--- scripts/modal-script.js
+++
@@ -1,9 +0,0 @@
-function openModal() {
-  document.body.classList.add('modal-open');
-  toggleBodyScroll(true);
-}
-
-function closeModal() {
-  document.body.classList.remove('modal-open');
-  toggleBodyScroll(false);
-}
